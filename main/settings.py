7c429f3148a5bbf40e19412ae7ae629e7d68d44f
Felix <donoasis88@gmail.com>
2024-09-10 20:59:39 +0100

    added some changes to the admin blog dashboard

--- main/settings.py
+++ main/settings.py
@@ -11,11 +11,11 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
