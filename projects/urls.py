d10b50956dc7b520670c8770d69e710fe9859710
Felix <donoasis88@gmail.com>
2024-09-17 17:32:24 +0000

    bug fixed and pages added

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
 path('projects/',views.projects,name='projects'),
-path('addproject/',views.add_project,name='add-project')
+path('addproject/',views.add_project,name='add-project'),
+path('modifyproject/',views.modifyproject,name='modify-project'),
+path('editproject/<int:project_id>/', views.edit_project, name='editproject'),  # For admin to edit blogs
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .models import Project
@@ -31,3 +31,63 @@ def add_project(request):
 
     # Render the form for adding a project
     return render(request, 'projects/addproject.html')
+
+
+
+# BLOGS FOR ADMIN
+@login_required(login_url = 'login')
+def modifyproject(request):
+    # Fetch all blog posts
+    project = Project.objects.all()
+    # Check if the user is authenticated and has the required email
+    if request.user.is_authenticated and request.user.email == 'felixsam922@gmail.com':
+        if request.method == 'POST':
+            if 'delete' in request.POST:
+                project_id = request.POST.get('delete')
+                project = get_object_or_404(Project, id=project_id)
+                project.delete()
+                return redirect(reverse('admin_dashboard'))
+            
+            elif 'edit' in request.POST:
+                project_id = request.POST.get('edit')
+                return redirect(reverse('editproject', args=[project_id]))
+    
+    # If user is not authorized, redirect to homepage
+    if not request.user.is_authenticated or request.user.email != 'felixsam922@gmail.com':
+        return redirect(reverse('homepage'))
+
+    # Render the blog posts template
+    return render(request, 'projects/modifyproject.html', {'projects': project})
+
+
+# EDIT Project FOR ADMIN 
+def edit_project(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    
+    if request.user.is_authenticated and request.user.email == 'felixsam922@gmail.com':
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            description = request.POST.get('description')
+            price = request.POST.get('price')
+            project_url = request.POST.get('project_url')
+            image = request.FILES.get('image')
+            
+            # Update project details
+            project.title = title
+            project.description = description
+            project.price = price
+            project.project_url = project_url
+            
+            # Update image if a new one is uploaded
+            if image:
+                project.image = image
+            
+            project.save()  # Save the changes
+            
+            return redirect(reverse('admin_dashboard'))
+    
+    # If user is not authorized, redirect to homepage
+    if not request.user.is_authenticated or request.user.email != 'felixsam922@gmail.com':
+        return redirect(reverse('homepage'))
+    
+    return render(request, 'projects/editproject.html', {'project': project})
