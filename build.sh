bc369c9a48c92ebf05e5a028328a04e1b51259ee
Felix <donoasis88@gmail.com>
2024-09-23 19:11:18 +0000

    added a super user to the hosted app

--- build.sh
+++ build.sh
@@ -17,16 +17,12 @@ SUPERUSER_USERNAME=Felix
 SUPERUSER_PASSWORD=Fe0546637494
 
 # Create a superuser account if it doesn't exist
-if [ -n "$SUPERUSER_EMAIL" ] && [ -n "$SUPERUSER_USERNAME" ] && [ -n "$SUPERUSER_PASSWORD" ]; then
-  python manage.py shell <<EOF
+python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='$SUPERUSER_USERNAME').exists():
+if not User.objects.filter(email='$SUPERUSER_EMAIL').exists():
     User.objects.create_superuser(username='$SUPERUSER_USERNAME', email='$SUPERUSER_EMAIL', password='$SUPERUSER_PASSWORD')
     print('Superuser created.')
 else:
-    print('Superuser already exists.')
+    print('Superuser with this email already exists.')
 EOF
-else
-  echo "Skipping superuser creation. Ensure SUPERUSER_EMAIL, SUPERUSER_USERNAME, and SUPERUSER_PASSWORD are set."
-fi
